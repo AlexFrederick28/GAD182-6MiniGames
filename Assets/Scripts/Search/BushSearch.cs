e4b9bf63ebe1cdd0a72ffbaa0d145207fcfba579
AlexFrederick28 <alexanderfrederick12@hotmail.com>
2024-10-10 14:08:58 +1100

    Searching Mechanics continued

--- Assets/Scripts/Search/BushSearch.cs
+++ Assets/Scripts/Search/BushSearch.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BushSearch : MonoBehaviour
 {
-    private Animator bushAnimation;
+    public Animator bushAnimation;
 
     private SpriteRenderer bushRenderActive;
+
+    [SerializeField] private bool startSearch = false;
+    [SerializeField] private bool isPressingSearch = false;
 
 
 
@@ -18,12 +22,40 @@ public class BushSearch : MonoBehaviour
             bushAnimation = GetComponent<Animator>();
         }
 
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isPressingSearch = true;
+        }
+
+        if (startSearch == false)
+        {
+            bushAnimation.Play("Idle");
+        }
+
+        if (isPressingSearch == true)
+        {
+            bushAnimation.Play("Large Pine Animation");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Colliding with " + collision.transform.name);
+
+        if (collision.gameObject.GetComponent<TopDownMovement>())
+        {
+            Debug.Log("start russel animation with " + collision.transform.name);
+
+            
+            startSearch = true;
+        }
+
     }
 }
